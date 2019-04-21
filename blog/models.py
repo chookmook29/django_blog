8d9740622c626d2e76c21b931a4eadd32b29ec04
Piotr Chudziak <strangelove44@gmail.com>
2019-04-21 22:35:40 +0100

    add reverse function

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
-class Post(models.Model):
-	title = models.CharField(max_length=200)
-	author = models.ForeignKey(
-		'auth.User',
-		on_delete=models.CASCADE,
-	)
-	body = models.TextField()
+from django.urls import reverse
 
-	def __str__(self):
-		return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
